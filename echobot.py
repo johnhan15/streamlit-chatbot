910db1f475dab0d3ae83514172e9870be91ba20c
johnhan15 <ijiu0412@gmail.com>
2026-04-01 18:32:42 +0900

    메세지 추가
    메세지 컨텐츠만듦

--- echobot.py
+++ echobot.py
@@ -2,11 +2,27 @@ import streamlit as st
 
 st.title(":blue[:material/smart_toy:] 에코봇")
 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
 
 
 if prompt := st.chat_input("에코봇에게 물어보기"):
     with st.chat_message("user"):
         st.write(prompt)
+        st.session_state.messages.append({
+                "role": "user",
+                "coutent": prompt            
+        })
 
     with st.chat_message("ai"):
         st.write(prompt)
+        st.session_state.messages.append({
+                "role": "ai",
+                "coutent": prompt            
+        })
